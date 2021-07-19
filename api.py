87da4efd966a7265c454f5467ce48f55abe7a79e
Mario Rivero <mariorivero051@gmail.com>
2021-07-19 18:30:41 +0000

    initial commit

---
+++ api.py
@@ -0,0 +1,14 @@
+import requests
+
+url = "https://webknox-recipes.p.rapidapi.com/recipes/findByIngredients"
+
+querystring = {"ingredients":"apples,flour,sugar","number":"5"}
+
+headers = {
+    'x-rapidapi-key': "b28cffec83msh01f2949c69f1af9p16ee98jsnb1cc8145b207",
+    'x-rapidapi-host': "webknox-recipes.p.rapidapi.com"
+    }
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+print(response.text)
---
+++ website/main.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__) # this gets the name of the file so Flask knows it's name 
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World</p>" 
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
